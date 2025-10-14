fdc68f40047bbcc80e0faba0b738ca3e89480584
Makarov628 <frostmakarov@gmail.com>
2025-10-14 10:14:16 +0500

    4) DynArray

--- obj/Debug/net9.0/oap-1.AssemblyInfo.cs
+++ obj/Debug/net9.0/oap-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9558e05660b6f199d41202c75c7f7662bbe92f83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59fcbe913da89375d9fecfd1533aa6b1f7870ad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
