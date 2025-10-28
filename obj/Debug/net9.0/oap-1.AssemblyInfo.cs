218f849d6f609acebdc5bf2cf598ed49ba60a47c
Makarov628 <frostmakarov@gmail.com>
2025-10-28 09:41:38 +0500

    5) reflection

--- obj/Debug/net9.0/oap-1.AssemblyInfo.cs
+++ obj/Debug/net9.0/oap-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c740bde2344a07c6f2d6a1b55721533c1a73fcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a83f27a7b40e42061201f32371c4b2ccb1b8fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
