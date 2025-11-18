559050ba30dc2f7d1b18fc0b09c68c446c99bdcb
Makarov628 <frostmakarov@gmail.com>
2025-11-18 23:36:59 +0500

    10) BloomFilter

--- obj/Debug/net9.0/oap-1.AssemblyInfo.cs
+++ obj/Debug/net9.0/oap-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465ee1101989c9e07061c55eea7e8447e7e4f293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf2b8b5978ebad71967ae58ea45e12149e69cd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oap-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
